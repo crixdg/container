e3766313a4c14314a6f427dd06ccd9a0889b4272
Crix Dang <crix.dang@gmail.com>
2026-05-08 16:52:01 +0700

    up Fri May  8 09:52:01 AM UTC 2026

--- k3s/scripts/uninstall.sh
+++ k3s/scripts/uninstall.sh
@@ -10,6 +10,7 @@ elif [ -f /usr/local/bin/k3s-agent-uninstall.sh ]; then
   /usr/local/bin/k3s-agent-uninstall.sh
 else
   echo "k3s uninstall script not found — is k3s installed on this host?"
+  exit 1
 fi
 
 echo "k3s removed."
