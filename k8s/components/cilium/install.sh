6e16625967d888faa34dd80bb67f4e62662014bb
Crix Dang <crix.dang@gmail.com>
2026-05-08 19:45:13 +0700

    up Fri May  8 12:45:13 PM UTC 2026

--- k8s/components/cilium/install.sh
+++ k8s/components/cilium/install.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-. "$SCRIPT_DIR/install_repo.sh"
-
 VALUE_FILE="$SCRIPT_DIR/values.yml"
 DEFAULT_VALUE_FILE="$SCRIPT_DIR/__default_values.yml"
 VERSION="$(cat $SCRIPT_DIR/version.conf)"
 
+. "$SCRIPT_DIR/install_repo.sh"
 
 if [ ! -f "$VALUE_FILE" ]; then
 	VALUE_FILE="$DEFAULT_VALUE_FILE"
 fi
-
 if [ ! -f "$VALUE_FILE" ]; then
 	echo "Not found: $VALUE_FILE"
 	exit 1
 fi
 
-helm install cilium cilium/cilium --version $VERSION -f "$VALUE_FILE" \
-	-n cni-system --create-namespace
+helm install cilium cilium/cilium --version $VERSION -f "$VALUE_FILE" -n cni-system --create-namespace
